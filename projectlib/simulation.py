1674ff87329e70c57037c22b9c0561af91a1c149
Charles Li <charles9000li@gmail.com>
2022-11-30 09:53:56 -0800

    simple LJ fluid system works

---
+++ projectlib/simulation.py
@@ -0,0 +1,145 @@
+"""simulation.py: Contains classes for running simulations and storing their
+states.
+"""
+__all__ = ['State', 'Simulation']
+
+import numpy as np
+
+from projectlib import utils
+
+
+def initialize_positions(num_atoms, box_length):
+    """Returns an array of initial positions of each atom, placed on a cubic
+    lattice for convenience.
+
+    Parameters
+    ----------
+    num_atoms : int
+        Number of particles.
+    box_length : float
+        Length of each side of the box
+
+    Returns
+    -------
+    positions : np.ndarray, shape=(num_atoms, 3)
+        Initialized positions
+    """
+    # initialize position array
+    positions = np.zeros((num_atoms, 3), dtype=float)
+
+    # compute integer grid # of locations for cubic lattice
+    NLat = int(num_atoms ** (1. / 3.) + 1.)
+    LatSpac = box_length / NLat
+    #make an array of lattice sites
+    r = LatSpac * np.arange(NLat, dtype=float) - 0.5 * box_length
+    #loop through x, y, z positions in lattice until done
+    #for every atom in the system
+    i = 0
+    for x in r:
+        for y in r:
+            for z in r:
+                positions[i] = np.array([x,y,z], float)
+                #add a random offset to help initial minimization
+                Offset = 0.1 * LatSpac * (np.random.rand(3) - 0.5)
+                positions[i] = positions[i] + Offset
+                i += 1
+                #if done placing atoms, return
+                if i >= num_atoms:
+                    return positions
+    return positions
+
+
+def initialize_velocities_from_temperature(num_atoms, temperature):
+    """Returns an initial random velocity set.
+
+    Parameters
+    ----------
+    num_atoms : int
+        Number of particles.
+    temperature : float
+        Target temperature
+
+    Returns
+    -------
+    velocities : np.ndarray, shape=(num_atoms, 3)
+    """
+    # randomly initialize velocities
+    velocities = np.random.rand(num_atoms, 3)
+
+    # scale velocities to target temperature
+    velocities = utils.rescale_velocities(velocities, temperature)
+
+    return velocities
+
+
+class State(object):
+    """Container object that stores information about the current state of a
+    simulation.
+
+    Attributes
+    ----------
+    positions : np.ndarray, shape=(num_particles, 3)
+        Current positions of all particles.
+    velocities : np.ndarray, shape=(num_particles, 3)
+        Current velocities of all particles.
+    forces : np.ndarray, shape=(num_particles, 3)
+        Current force acting on all particles.
+    potential_energy : float
+        Current potential energy of the system.
+    kinetic_energy : float
+        Current kinetic energy of the system.
+    """
+
+    def __init__(self, positions, velocities, forces, potential_energy, kinetic_energy):
+        self.positions = positions
+        self.velocities = velocities
+        self.forces = forces
+        self.potential_energy = potential_energy
+        self.kinetic_energy = kinetic_energy
+
+
+class Simulation(object):
+
+    def __init__(self, system, integrator):
+        self.system = system
+        self.integrator = integrator
+        self.integrator.simulation = self
+        self.state = None
+
+    def initialize(self):
+        positions = initialize_positions(self.system.n_beads, self.system.box_lengths[0])
+        velocities = initialize_velocities_from_temperature(self.system.n_beads, 1.0)
+        forces, potential_energy = self.system.force_energy_function(positions)
+        kinetic_energy = utils.compute_kinetic_energy(velocities)
+        self.state = State(positions, velocities, forces, potential_energy, kinetic_energy)
+
+    def step(self, steps):
+        steps_elapsed = 0
+        thermo_frequency = 1000
+        while steps > 0:
+            if steps > thermo_frequency:
+                steps_to_take = thermo_frequency
+            else:
+                steps_to_take = steps
+            state = self.integrator.step(steps_to_take)
+            steps -= steps_to_take
+            steps_elapsed += steps_to_take
+            self.state = state
+            temperature = utils.compute_instantaneous_temperature(state.velocities)
+            print(steps_elapsed, state.potential_energy, state.kinetic_energy, temperature)
+
+
+if __name__ == '__main__':
+    from projectlib.components import BeadType, Chain
+    from system import System
+    from projectlib.integrators import LangevinIntegratorLAMMPS
+
+    bead_type = BeadType("A")
+    molecule = Chain("A", ["A"])
+    system = System(box_lengths=np.array([5, 5, 5]))
+    system.add_bead_type(bead_type)
+    system.add_chain(molecule, num=100)
+    integrator = LangevinIntegratorLAMMPS()
+    simulation = Simulation(system, integrator)
+    simulation.initialize()
+    simulation.step(100000)
---
+++ projectlib/system.py
@@ -0,0 +1,132 @@
+import numpy as np
+from numba import jit
+
+
+class System(object):
+
+    def __init__(self, box_lengths, cutoff=2.5):
+        self.force_field = None
+        self.box_lengths = box_lengths
+        self.cutoff = cutoff
+        self._bead_types = {}
+        self._chains = {}
+        self._interactions = {}
+
+        self._num_beads = None
+        self._bead_species = None
+        self._bead_species_indices = None
+        self._chain_names = None
+        self._chain_indices = None
+        self._force_energy_function = None
+
+    def add_bead_type(self, bead_type):
+        self._bead_types[bead_type.name] = bead_type
+
+    def get_bead_type(self, bead_name):
+        return self._bead_types[bead_name]
+
+    def add_chain(self, chain, num=1):
+        self._chains[chain.name] = [chain, num]
+
+    def get_chain(self, chain_name):
+        return self._chains[chain_name][0]
+
+    def get_chain_num(self, chain_name):
+        return self._chains[chain_name][1]
+
+    def set_chain_num(self, chain_name, num=1):
+        self._chains[chain_name][1] = num
+
+    @property
+    def n_beads(self):
+        return sum([chain.n_beads * num for chain, num in self._chains.values()])
+
+    @property
+    def bead_types(self):
+        return self._bead_types.values()
+
+    @property
+    def bead_names(self):
+        return self._bead_types.keys()
+
+    @property
+    def all_bead_names(self):
+        all_bead_names = []
+        for chain, num in self._chains.values():
+            for _ in range(num):
+                all_bead_names.extend(chain.bead_names)
+        return np.array(all_bead_names, dtype=str)
+
+    @property
+    def all_chain_names(self):
+        all_chain_names = []
+        for chain, num in self._chains.values():
+            for _ in range(num):
+                all_chain_names.append(chain.name)
+        return np.array(all_chain_names, dtype=str)
+
+    def create_force_energy_function(self):
+        # loop through molecules and create lists of bead names and bonds
+        bead_names = []
+        chain_names = []
+        chain_indices = []
+        all_bonds = []
+        current_chain_index = 0
+        for chain, num in self._chains.values():
+            chain_names.append(chain.name)
+            for _ in range(num):
+                num_beads_curr = len(bead_names)
+                bead_names.extend(chain.bead_names)
+                chain_indices.append(current_chain_index)
+                all_bonds.extend(map(lambda x: (x[0]+num_beads_curr, x[1]+num_beads_curr), chain.bonds))
+                current_chain_index += 1
+        bead_names = np.array(bead_names, dtype=str)
+        forcefield_bead_names = list(self.bead_names)
+        bead_species_indices = np.array([forcefield_bead_names.index(bn) for bn in bead_names], dtype=int)
+        print(bead_species_indices)
+        print(chain_indices)
+
+        # create bonded matrix
+        bonded_matrix = np.full((self.n_beads, self.n_beads), False)
+        for bond in all_bonds:
+            i, j = bond
+            bonded_matrix[i, j] = bonded_matrix[j, i] = True
+
+        cutoff = self.cutoff
+        box_lengths = self.box_lengths
+        num_beads = self.n_beads
+
+        @jit(nopython=True)
+        def force_energy_function(positions):
+            cutoff_sqd = cutoff * cutoff
+            shift = -4. * (cutoff**-12 - cutoff**-6)
+            forces = np.zeros_like(positions)
+            potential_energy = 0.0
+            for i in range(num_beads):
+                position_i = positions[i, :]
+                r_ij_array = positions[:i, :] - position_i
+                r_ij_array = r_ij_array - box_lengths * np.rint(r_ij_array / box_lengths)
+                d_sqd_array = np.sum(r_ij_array * r_ij_array, axis=1)
+                for j in range(i):
+                    d_sqd = d_sqd_array[j]
+                    r_ij = r_ij_array[j]
+                    if d_sqd > cutoff_sqd:
+                        continue
+                    inv_d2 = 1. / d_sqd
+                    inv_d6 = inv_d2 * inv_d2 * inv_d2
+                    inv_d12 = inv_d6 * inv_d6
+                    force_ij = r_ij * ((-48. * inv_d12 + 24. * inv_d6) * inv_d2)
+                    forces[i, :] += force_ij
+                    forces[j, :] -= force_ij
+                    potential_energy += 4. * (inv_d12 - inv_d6) + shift
+
+            return forces, potential_energy
+
+        self._force_energy_function = force_energy_function
+        return force_energy_function
+
+    @property
+    def force_energy_function(self):
+        if self._force_energy_function is None:
+            self.create_force_energy_function()
+        return self._force_energy_function
