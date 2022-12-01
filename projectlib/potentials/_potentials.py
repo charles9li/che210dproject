49a883f45e5e4e04476a1e7edf618d2e03a3eaf9
Charles Li <charles9000li@gmail.com>
2022-12-01 12:08:08 -0800

    initial commit

---
+++ projectlib/potentials/_potentials.py
@@ -0,0 +1,389 @@
+"""_potentials.py: Contains various base classes that all potentials inherit from."""
+__all__ = ['_PairPotential', '_BondedPairPotential']
+
+from collections import defaultdict
+import itertools
+
+import numpy as np
+
+from numba import jit
+
+
+# =========================================================================
+# Utility functions used for working with nested dictionaries
+# =========================================================================
+
+def _create_empty_parameter_dict(n_beads, n_parameters):
+    if n_beads == 1:
+        return defaultdict(lambda: np.zeros(n_parameters))
+    return defaultdict(lambda: _create_empty_parameter_dict(n_beads-1, n_parameters))
+
+
+def _get_value_from_nested_dict(nested_dict, *keys):
+    if len(keys) == 1:
+        return nested_dict[keys[0]]
+    return _get_value_from_nested_dict(nested_dict[keys[0]], *keys[1:])
+
+
+def _set_value_of_nested_dict(nested_dict, value, *keys):
+    if len(keys) == 1:
+        nested_dict[keys[0]] = value
+        return
+    _set_value_of_nested_dict(nested_dict[keys[0]], value, *keys[1:])
+
+
+def _get_key_groups_from_nested_dict(nested_dict, depth):
+    key_groups = []
+    for key in nested_dict.keys():
+        if depth == 1:
+            key_groups.append([key])
+        else:
+            for kg in _get_key_groups_from_nested_dict(nested_dict, depth-1):
+                key_groups.append([key] + list(kg))
+    return list(map(lambda x: tuple(x), key_groups))
+
+
+# =========================================================================
+# _Potential base class definition
+# =========================================================================
+
+class _Potential(object):
+    """Base class for all potentials.
+
+    Attributes
+    ----------
+    N_BEADS : int
+        Class attribute that specifies the number of beads involved in each
+        interaction.
+    BONDED : bool
+        Specifies whether this potential is bonded. If True, then the order of
+        the bead types matters when specifying interactions.
+    PARAMETERS : list of str
+        List of independent parameters for each interaction.
+    DEPENDENT_PARAMETERS : list of str
+        List of parameters that depend on the independent parameters.
+    ALL_PARAMETERS : list of str
+        List of all parameters in the potential. The parameter indices
+        correspond to their position in this list.
+    """
+    N_BEADS = 1
+    BONDED = False
+    PARAMETERS = []
+    DEPENDENT_PARAMETERS = []
+    ALL_PARAMETERS = PARAMETERS + DEPENDENT_PARAMETERS
+
+    def __init__(self):
+        self.force_field = None
+        self._parameter_dict = _create_empty_parameter_dict(self.N_BEADS, len(self.ALL_PARAMETERS))
+
+        # initialize attributes that are only set after compilation
+        self._if_compute = None
+        self._parameter_array = None
+        self._force_energy_function = None
+
+    @property
+    def n_parameters(self):
+        """Number of parameters in this potential."""
+        return len(self.ALL_PARAMETERS)
+
+    def _initialize_interaction(self, *bead_names):
+        """Private helper method that initializes an entry in the parameter
+        dict.
+
+        Parameters
+        ----------
+        *bead_names : tuple of str
+            Variable length tuple of bead names.
+        """
+        # initialize one entry get retrieving parameter array
+        _parameter_list = self._get_parameter_list(*bead_names)
+
+        # if bonded, order of bead names matters
+        if self.BONDED:
+            _set_value_of_nested_dict(self._parameter_dict, _parameter_list, *bead_names)
+            _set_value_of_nested_dict(self._parameter_dict, _parameter_list, *reversed(bead_names))
+        # if non bonded, order doesn't matter
+        else:
+            for p in set(itertools.permutations(bead_names)):
+                _set_value_of_nested_dict(self._parameter_dict, _parameter_list, *p)
+
+    def add_interaction(self, *bead_names, **kwargs):
+        """Adds an interaction between specified bead types to this potential.
+
+        Parameters
+        ----------
+        *bead_names : tuple of str
+            Variable length tuple of bead names.
+        **kwargs : dict of str: float
+            Values of parameters in the interaction.
+        """
+        # check that provided number of bead names matches the potential
+        self._check_n_bead_names(*bead_names)
+
+        # retrieve array to initialize entries in the parameter dict
+        self._initialize_interaction(*bead_names)
+
+        # set values of all parameters specified in kwargs
+        for parameter_name, value in kwargs.items():
+            self.set_parameter_value(parameter_name, value, *bead_names, update_dependents=False)
+
+        # update values of dependent parameters
+        self._update_dependent_parameters(*bead_names)
+
+    def has_interaction(self, *bead_names):
+        """Checks if this potential has an interaction between the specified
+        bead names.
+
+        Parameters
+        ----------
+        *bead_names : tuple of str
+            Variable length tuple of bead names.
+        """
+        # check that provided number of bead names matches the potential
+        self._check_n_bead_names(*bead_names)
+
+        # recursively go through nested parameter dict
+        curr_object = self._parameter_dict
+        while isinstance(curr_object, defaultdict):
+            if bead_names[0] not in curr_object.keys():
+                return False
+            curr_object = curr_object[bead_names[0]]
+            bead_names = bead_names[1:]
+        return True
+
+    def has_parameter(self, parameter_name):
+        """Returns true if this potential has a parameter of the specified
+        name.
+
+        Parameters
+        ----------
+        parameter_name : str
+            Name of the parameter.
+
+        Returns
+        -------
+        bool
+            True if this potential has the specified parameter.
+        """
+        return parameter_name in self.ALL_PARAMETERS
+
+    def set_parameter_value(
+            self, parameter_name, new_value, *bead_names,
+            update_dependents=True
+    ):
+        """Set value of a parameter for an interaction between the specified
+        bead types.
+
+        Parameters
+        ----------
+        parameter_name : str
+            Name of the parameter.
+        new_value : float
+            New value of the parameter.
+        *bead_names : str
+            Variable length tuple of bead names.
+        update_dependents : bool, default=True
+            Flag that specifies whether dependent parameters should be updated.
+            Should always be True unless you know what you're doing.
+        """
+        # check that provided number of bead names matches the potential
+        self._check_n_bead_names(*bead_names)
+
+        # check that parameter exists in this potential
+        self._raise_no_parameter_error(parameter_name)
+
+        # raise error if no interaction exists for specified bead names
+        self._raise_no_interaction_error(*bead_names)
+
+        # can't set values of dependent parameters unless forcibly overriden
+        if update_dependents and parameter_name in self.DEPENDENT_PARAMETERS:
+            raise ValueError("can't set values of dependent parameters")
+
+        # get parameter array and set new value of parameter
+        _parameter_array = self._get_parameter_list(*bead_names)
+        _parameter_index = self._get_parameter_index(parameter_name)
+        _parameter_array[_parameter_index] = new_value
+
+        # update dependent parameters unless forcibly overriden
+        if update_dependents:
+            self._update_dependent_parameters(*bead_names)
+
+    def get_parameter_value(self, parameter_name, *bead_names):
+        """Get value of a parameter for an interaction between the specified
+        bead types.
+
+        Parameters
+        ----------
+        parameter_name : str
+            Name of the parameter.
+        *bead_names : str
+            Variable length tuple of bead names.
+
+        Returns
+        -------
+        float
+            Value of the parameter.
+        """
+        # check that provided number of bead names matches the potential
+        self._check_n_bead_names(*bead_names)
+
+        # check that parameter exists in this potential
+        self._raise_no_parameter_error(parameter_name)
+
+        # raise error if no interaction exists for specified bead names
+        self._raise_no_interaction_error(*bead_names)
+
+        # get parameter array and index and return value
+        _parameter_list = self._get_parameter_list(*bead_names)
+        _parameter_index = self._get_parameter_index(parameter_name)
+        return _parameter_list[_parameter_index]
+
+    def _get_parameter_list(self, *bead_names):
+        """Private helper method that retrieves the parameter array for a
+        specific interaction.
+
+        Parameters
+        ----------
+        *bead_names : tuple of str
+            Variable length tuple of bead names.
+
+        Returns
+        -------
+        np.ndarray, shape=(self.n_parameters,)
+            Parameter array corresponding to the specified interaction.
+        """
+        return _get_value_from_nested_dict(self._parameter_dict, *bead_names)
+
+    def _get_parameter_index(self, parameter_name):
+        """Private helper method that index for a specified parameter.
+
+        Parameters
+        ----------
+        parameter_name : str
+            Name of the parameter.
+
+        Returns
+        -------
+        int
+            Index of the parameter.
+        """
+        return self.ALL_PARAMETERS.index(parameter_name)
+
+    def _update_dependent_parameters(self, *bead_names):
+        """Private helper method that updates the values of all dependent
+        parameters for a specified interaction.
+
+        Parameters
+        ----------
+        *bead_names : tuple of str
+            Variable length tuple of bead names.
+        """
+        pass
+
+    def _check_n_bead_names(self, *bead_names):
+        """Raises an error if number of provided bead names doesn't match the
+        number of bead types in each interaction for this potential.
+
+        Parameters
+        ----------
+        *bead_names : tuple of str
+            Variable length tuple of bead names.
+
+        Raises
+        ------
+        ValueError
+            If there is a mismatch between the number of bead names specified
+            and the number of bead types involved in each interaction.
+        """
+        if len(bead_names) != self.N_BEADS:
+            raise ValueError(
+                f"number of bead names provided does not match number of beads "
+                f"involved in this interaction, which is {self.N_BEADS}"
+            )
+
+    def _raise_no_interaction_error(self, *bead_names):
+        """Raises an error if no interaction exists between the specified bead
+        names in this potential.
+
+        Parameters
+        ----------
+        *bead_names
+            Variable length tuple of bead names.
+
+        Raises
+        ------
+        ValueError
+            When no interaction exists between the specified bead names.
+        """
+        if not self.has_interaction(*bead_names):
+            raise ValueError(f"no interaction exists between species ({', '.join(bead_names)}) in this potential")
+
+    def _raise_no_parameter_error(self, parameter_name):
+        """Raises an error if no parameter with the specified name exists in
+        this potential.
+
+        Parameters
+        ----------
+        parameter_name : str
+            Name of the parameter.
+
+        Raises
+        ------
+        ValueError
+            When no interaction exists between the specified bead names.
+        """
+        if not self.has_parameter(parameter_name):
+            raise ValueError(f"no parameter '{parameter_name}' exists in this potential")
+
+    def create_force_energy_function(self):
+        """Factory method that creates a function to calculate the force and
+        energy due to this potential. Child classes will override this method.
+        """
+        @jit(nopython=True)
+        def calculate_force_energy(forces, potential_energy):
+            return forces, potential_energy
+        self._force_energy_function = calculate_force_energy
+        return calculate_force_energy
+
+    @property
+    def force_energy_function(self):
+        """Numba-accelerated function that computes the force and energy due
+        to this potential."""
+        if self._force_energy_function is None:
+            self.create_force_energy_function()
+        return self._force_energy_function
+
+    def _create_if_compute_and_parameter_arrays(self):
+        """Private helper method that creates arrays used for the force energy
+        function.
+        """
+        # get data from force field
+        bead_names = list(self.force_field.bead_names)
+        n_bead_types = self.force_field.n_bead_types
+
+        # initialize arrays
+        if_compute = np.full([n_bead_types]*self.N_BEADS, False, dtype=bool)
+        parameter_array = np.zeros([n_bead_types]*self.N_BEADS + [len(self.ALL_PARAMETERS)])
+
+        # fill in arrays
+        for key_group in _get_key_groups_from_nested_dict(self._parameter_dict, self.N_BEADS):
+            species_indices = tuple([bead_names.index(bn) for bn in key_group])
+            if_compute[species_indices] = True
+            parameter_list = self._get_parameter_list(*key_group)
+            parameter_array[species_indices] = parameter_list
+
+        # save to attributes
+        self._if_compute = if_compute
+        self._parameter_array = parameter_array
+
+
+# =========================================================================
+# Other base classes for convenience
+# =========================================================================
+
+class _PairPotential(_Potential):
+    N_BEADS = 2
+
+
+class _BondedPairPotential(_PairPotential):
+    BONDED = True
